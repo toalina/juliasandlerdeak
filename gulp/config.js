cb1c3f65651bfd1ec98b3adc5d22171881b794f5
Alina To <alina.to@gmail.com>
2015-11-01 15:48:12 -0800

    Refactor entire website with gulp, sass, and node modules

---
+++ gulp/config.js
@@ -0,0 +1,35 @@
+var src = "./lib";
+var dest = "./public";
+
+module.exports = {
+  javascript: {
+    src: src + '/js/**/*.js',
+    dest: dest + '/js/',
+    entry: src + '/js/entry.js',
+    outputFilename: 'packed.js'
+  },
+  sass: {
+    src: src + "/sass/{,*/}*.{scss,sass}",
+    dest: dest + '/css/',
+  },
+  index: {
+    src: src + "/index.html",
+    dest: dest + "/"
+  },
+  html: {
+    src: src + "/views/**/*.html",
+    dest: dest + "/views/"
+  },
+  server: {
+    src: dest,
+    livereload: true,
+    directoryListing: false,
+    open: false,
+    port: 9000
+  },
+  production: {
+    cssSrc: dest + '/css/*.css',
+    jsSrc: dest + '/js/*.js',
+    dest: dest
+  }
+};
---
+++ gulp/tasks/sass.js
@@ -0,0 +1,20 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var handleErrors = require('../util/handleErrors');
+var autoprefixer = require('gulp-autoprefixer');
+var config = require('../config').sass;
+
+gulp.task('sass', function () {
+  return gulp.src(config.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(config.settings))
+    .on('error', handleErrors)
+    .pipe(sourcemaps.write())
+
+    // .pipe(autoprefixer({ browsers: ['last 2 version'], cascade: false }))
+
+    .pipe(gulp.dest(config.dest));
+});
+
+
